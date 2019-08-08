2f4bc3660b2256dda2beb1759a923da0b00de510
JHK <hk.jung@birdview.kr>
2019-08-08 15:34:07 +0900

    Test

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.test
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    init {
+        btn_submit.setOnClickListener {
+            tv_main.text = "Submit!"
+        }
     }
 }
